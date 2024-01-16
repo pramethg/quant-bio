7d219a7296c54d86dd7055307f9375f5359cf37f
Prameth Gaddale <pramethg@gmail.com>
2024-01-16 14:24:33 -0500

    minimal changes, added docs for reversible reaction

--- revrxn.m
+++ revrxn.m
@@ -9,20 +9,18 @@ ca0 = 1;
 kf = 1;
 kr = 0.5;
 time = t0:dt:tend;
-afwd = zeros(6, size(time, 2));
-arev = zeros(6, size(time, 2));
-expconc = zeros(8, size(time, 2));
+conc = NaN(8, size(time, 2));
 errors = NaN(6, size(time, 2));
 
 %%
-expconc(1, :) = ca0 * (kr + kf*exp(-(kf + kr)*time)) / (kr + kf);
-expconc(2, :) = ca0 * (kf - kf*exp(-(kf + kr)*time)) / (kr + kf);
-expconc(3:4, :) = rk1([kf, kr], t0, tend, dt, [ca0, 0], 1);
-expconc(5:6, :) = rk2([kf, kr], t0, tend, dt, [ca0, 0], 1);
-expconc(7:8, :) = rk4([kf, kr], t0, tend, dt, [ca0, 0], 1);
+conc(1, :) = ca0 * (kr + kf*exp(-(kf + kr)*time)) / (kr + kf);
+conc(2, :) = ca0 * (kf - kf*exp(-(kf + kr)*time)) / (kr + kf);
+conc(3:4, :) = rk1([kf, kr], t0, tend, dt, [ca0, 0], 1);
+conc(5:6, :) = rk2([kf, kr], t0, tend, dt, [ca0, 0], 1);
+conc(7:8, :) = rk4([kf, kr], t0, tend, dt, [ca0, 0], 1);
 
 for i = 0:2
-  errors(2*i+1:2*i+2, :) = abs(expconc(1:2, :) - expconc(2*i+3:2*i+4, :));
+  errors(2*i+1:2*i+2, :) = abs(conc(1:2, :) - conc(2*i+3:2*i+4, :));
 end
 
 %%
@@ -30,14 +28,15 @@ colors = ["b", "g"];
 labels = ["Time", "Concentration", "Error"];
 
 figure;
+sgtitle(["First Order Reversible Reaction Concentration"])
 subplot(2, 4, 1)
-plotconc(time, expconc(1:2, :), labels(1:2), colors, "Analytical Solution");
+plotconc(time, conc(1:2, :), labels(1:2), colors, "Analytical Solution");
 subplot(2, 4, 2)
-plotconc(time, expconc(3:4, :), labels(1:2), colors, "Euler's Method Solution");
+plotconc(time, conc(3:4, :), labels(1:2), colors, "Euler's Method Solution");
 subplot(2, 4, 3)
-plotconc(time, expconc(5:6, :), labels(1:2), colors, "Runge Kutta 2nd Order Solution");
+plotconc(time, conc(5:6, :), labels(1:2), colors, "Runge Kutta 2nd Order Solution");
 subplot(2, 4, 4)
-plotconc(time, expconc(7:8, :), labels(1:2), colors, "Runge Kutta 4th Order Solution");
+plotconc(time, conc(7:8, :), labels(1:2), colors, "Runge Kutta 4th Order Solution");
 ploterrror(time, errors, ["Euler's", "RK-2", "RK-4"], labels([1, 3]));
 
 %%

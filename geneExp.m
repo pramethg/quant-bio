6d9cfd1e7a8eac8e916d6e2f4eb262f751e546a2
pramethg <pramethg@gmail.com>
2024-03-11 21:00:19 -0400

    added changes, histograms

--- geneExp.m
+++ geneExp.m
@@ -6,85 +6,89 @@ tspan = [0 tstop];
 time = []; % Array to store the Time Values
 time(1) = tspan(1); % Initializing with 0
 Xi = [0 0]; % Iniitial Concentration Vector for mRNA and Protein
-X = zeros(length(Xi), tstop); % Counts Matrix Zero-Initialization
-X(:, 1) = Xi; % Initializing the First Time Step with Initial Counts
-b = 10; % Burst Size: b = k_p / gamma_r
-k_r = 0.01; % Transcription Rate
+X_Values = {}; % Cell to Store Count Matrices, 2 Matrices for 2 Different b values.
+T_Values = {}; % Cell to Store Time Matrices
+b = [10, 1]; % Burst Size: b = k_p / gamma_r
+k_r = [0.01 0.1]; % Transcription Rates
 gamma_r = 0.1; % mRNA Decay Rate
 gamma_p = 0.002; % Protein Decay Rate
 k_p = b * gamma_r; % Translation Rate
-C = [k_r k_p gamma_r gamma_p]; % Rate Constants Vector for Transcription, Translation, mRNA Decay, Protein Decay
-H = update_ch(C, Xi);
-Ch = C.*H'; % Compute the Initial Ch Vector
-i = 2; % Iteration Variable
-t = 0; % Time Variable used for deciding the stopping condition
-tau_arr = []; % Vector to store Tau-values
 colors = ["blue", "magenta", "red", "cyan"];
-labels = ["Time", "Number of mRNA/Protein", "Error"];
+labels = ["Time (Hours)", "Number of mRNA/Protein", "Error"];
 
 %% Main Loop for Direct Method Solution
-while (t < tstop)
-  % Generate Random Real Numbers(Between 0 and 1)
-  r1 = rand();
-  r2 = rand();
-  A = sum(Ch(:));
-  % Compute Tau Value
-  tau = 1 / (A + 1e-5) * log(1 / (r1 + 1e-5));
-  % Store Tau Value
-  tau_arr(i - 1) = tau;
-  % Compute Mu Value
-  mu = find((cumsum(Ch) >= r2 * A), 1, 'first');
-  % If suitable Mu is not found, just continue with another set of Tau
-  if isempty(mu)
-    continue
-  else
-    % Carry out the Reaction Based on Mu, Update Counts
-    X(:, i) = reaction(X(:, i - 1), mu);
-    % Update the Ch Matrix
-    [H, Ch] = update_ch(C, X(:, i));
-    i = i + 1;
-    t = t + tau;
-    time(i) = t;
+for idx = 1:2
+  % Initializing Variables for Direct Method Simulation
+  i = 2; % Iteration Variable
+  t = 0; % Time Variable used for deciding the stopping condition
+  C = [k_r(idx) k_p(idx) gamma_r gamma_p]; % Rate Constants Vector for Transcription, Translation, mRNA Decay, Protein Decay
+  H = update_ch(C, Xi);
+  Ch = C .* H'; % Compute the Initial Ch Vector
+  tau_arr = []; % Vector to store Tau-values
+  X = zeros(length(Xi), tstop); % Zero-Initialization of Counts Matrix
+  X(:, 1) = Xi; % Initializing the First Time Step with Initial Counts
+
+  % Loop for Direct Method Calculation
+  while (t < tstop)
+    % Generate Random Real Numbers(Between 0 and 1)
+    r1 = rand();
+    r2 = rand();
+    A = sum(Ch(:));
+    % Compute Tau Value
+    tau = 1 / (A + 1e-5) * log(1 / (r1 + 1e-5));
+    % Store Tau Value
+    tau_arr(i - 1) = tau;
+    % Compute Mu Value
+    mu = find((cumsum(Ch) >= r2 * A), 1, 'first');
+    % If suitable Mu is not found, just continue with another set of Tau
+    if isempty(mu)
+      continue
+    else
+      % Carry out the Reaction Based on Mu, Update Counts
+      X(:, i) = reaction(X(:, i - 1), mu);
+      % Update the Ch Matrix
+      [H, Ch] = update_ch(C, X(:, i));
+      i = i + 1;
+      t = t + tau;
+      time(i) = t;
+    end
   end
+  X = X(:, 1:size(time, 2) - 1);
+  X_Values{idx} = X;
+  T_Values{idx} = time(1: end - 1) / 3600; % Convert to Hours
 end
-X = X(:, 1:size(time, 2) - 1);
+
 %% ODE-45 Solver Solution
-[sGeneTime, sGeneConc] = ode45(@(t, y) geneExpression(t, y, C), time, Xi);
-% Error w.r.t. Direct Method and ODE-45 Solution
-error = X' - sGeneConc(1:end - 1, :);
+odeCountValues = {};
+odeTimeValues = {};
+for idx = 1:2
+  C = [k_r(idx) k_p(idx) gamma_r gamma_p]; % Rate Constants Vector for Transcription, Translation, mRNA Decay, Protein Decay
+  % ODE-45 Solution
+  [sGeneTime, sGeneConc] = ode45(@(t, y) geneExpression(t, y, C), linspace(0, max(T_Values{idx}(:)) * 3600, size(T_Values{idx}, 2)), Xi);
+  % Error w.r.t. Direct Method and ODE-45 Solution
+  error = X_Values{idx}' - sGeneConc;
+  odeCountValues{idx} = sGeneConc;
+  odeTimeValues{idx} = sGeneTime;
+end
 
 %% Plot the Direct Method Solution
-figure;
-plot(time(:, 1:end - 1), X(1, :), "Color", "blue");
-hold on
-plot(time(:, 1:end - 1), X(2, :), "Color", "green");
-legend(["#mRNA", "#Protein"]);
-xlabel("Time");
-ylabel("Number of mRNA/Protein");
-ylim([0, 120]);
-title("Direct Method");
+figure(1);
+subplot(1, 2, 1)
+matPlot(T_Values{1}, X_Values{1}, colors(1:2), labels(1:2), "Direct Method Solution (b = 10)", ["#mRNA", "#Protein"], [0, 150]);
+subplot(1, 2, 2)
+matPlot(T_Values{2}, X_Values{2}, colors(1:2), labels(1:2), "Direct Method Solution (b = 1)", ["#mRNA", "#Protein"], [0, 150]);
 
 %% Plot Analyzing Differences Betweem Direct Method and ODE-45 Solution
+clc;
 figure;
 subplot(2, 2, 1)
-matPlot(time(:, 1:end - 1), X, colors(1:2), labels(1:2), "Direct Method Solution", ["#mRNA", "#Protein"], [0, 120]);
+matPlot(T_Values{1}, cat(1, X_Values{1}(2, :), odeCountValues{1}(:, 2)'), colors([3, 1]), ["Time", "Protein Number"], "Direct Method vs. ODE-45 Solution (b = 10)", ["Direct Method", "ODE-45"], [0, 150]);
 subplot(2, 2, 2)
-matPlot(sGeneTime, sGeneConc', colors(1:2), labels(1:2), "ODE45 Solution", ["#mRNA", "#Protein"], [0, 120]);
+matPlot(T_Values{2}, cat(1, X_Values{2}(2, :), odeCountValues{2}(:, 2)'), colors([3, 1]), ["Time", "Protein Number"], "Direct Method vs. ODE-45 Solution (b = 1)", ["Direct Method", "ODE-45"], [0, 150]);
 subplot(2, 2, 3)
-matPlot(time(:, 1:end - 1), cat(1, X(2, :), sGeneConc(1:end - 1, 2)'), colors([3, 1]), ["Time", "Protein Number"], "Direct Method vs. ODE-45 Solution", ["Direct Method", "ODE-45"], [0, 120]);
+plotHistogram(X_Values{1}(2, :), "Histogram of Protein Number (b = 10)");
 subplot(2, 2, 4)
-matPlot(time(:, 1:end - 1), error', colors([3, 1]), labels([1, 3]), strcat("Sum of Errors(ODE45): ", num2str(sum(abs(error(:))))));
-
-%% Plot Analyzing Tau-Value With Respect to Time
-figure;
-plot(1:length(tau_arr), tau_arr, Color = "blue");
-hold on;
-plot(1:length(tau_arr), movmean(tau_arr, 10), Color = "green");
-xlabel("Iteration #");
-ylabel("Tau (a.u.)");
-title(strcat("Tau Value, Average: ", num2str(mean(tau_arr))));
-xlim([0 length(tau_arr) + 20]);
-legend(["Tau Value", "Moving Averaged(10) Tau Value"])
+plotHistogram(X_Values{2}(2, :), "Histogram of Protein Number (b = 1)");
 
 %% Function for Gene Expression Reactions
 function Xv = reaction(Xv, mu)
@@ -129,4 +133,14 @@ function matPlot(x, y, colors, labels, stitle, varargin)
     legend(["#mRNA", "#Protein"]);
   end
   hold off;
+end
+
+%% Plotting Histograms
+function plotHistogram(X, stitle)
+  indices = X > 10;
+  filteredCounts = X(indices);
+  histogram(filteredCounts, 'FaceColor', 'red');
+  xlabel('Protein Number Bins');
+  ylabel('Counts');
+  title(stitle);
 end

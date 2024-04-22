070c25c6a5d6bff37c8c91f6266cdd88ee8ffee7
pramethg <pramethg@gmail.com>
2024-04-22 11:06:05 -0400

    added the code for python based MCMC model generation

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def argparser():
   parser.add_argument("--parameters", type = str, default = "80, 0.15, 0.00002, 0.55, 900, 5.5")
   parser.add_argument("--initial", type = str, default = "50, 600, 50000")
   parser.add_argument("--tspan", type = int, default = 60)
+  parser.add_argument("--niters", type = int, default = 10000)
+  parser.add_argument("--stepsize", type = float, default = 3e-4)
   return parser
 
 def ode(args):
@@ -19,6 +21,7 @@ def ode(args):
   sol = solve_ivp(lambda t, Y: covidModel(t, Y, parameters), tspan, Y0, dense_output = True)
   t = np.linspace(tspan[0], tspan[1], 300)
   Y = sol.sol(t)
+  data = dataExp('./data/expCells.csv', 0)
   titles = ["T (Uninfected T Cells)", "I (Infected Virus Cells)", "logV (Virus Particles)"]
   colors = ["b", "g", "r"]
   plt.figure(figsize = (15, 5))
@@ -28,11 +31,26 @@ def ode(args):
       plt.plot(t, np.abs(np.log(Y[idx])), color = colors[idx])
     else:
       plt.plot(t, Y[idx], color = colors[idx])
+    plt.scatter(data[:, 0], data[:, idx], c = colors[idx])
     plt.title(titles[idx])
     plt.xlabel("Time")
     plt.ylabel("Population Count")
+    plt.legend(['ODE', 'Experimental'])
   plt.suptitle("COVID Population Dynamics")
   plt.tight_layout()
+  plt.show()
+
+def mcmc(args):
+  data = dataExp("./data/expCells.csv", 0)
+  initParam = [0.00002, 0.15, 0.55, 5.5, 80, 900]
+  paramNames = ['beta', 'rho', 'gamma', 'lambda', 'c', 'n']
+  nParams = len(initParam)
+  (paramChain, acceptChain) = runMCMC(data, initParam, args.niters, args.stepsize)
+  plt.figure(figsize = (5, 20))
+  for idx in range(nParams):
+    plt.subplot(nParams, 1, idx + 1)
+    plt.plot(paramChain[:, idx])
+    plt.title(f"Parameter - {paramNames[idx].upper()}")
   plt.show()
 
 def train(args):
@@ -42,5 +60,7 @@ if __name__ == "__main__":
   args = argparser().parse_args()
   if args.model == "ode":
     ode(args)
+  elif args.model == "mcmc":
+    mcmc(args)
   elif args.model == "":
     train(args)
--- utils/models.py
+++ utils/models.py
@@ -1,3 +1,6 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+
 def covidModel(t, Y, parameters):
   T, I, V = Y
   lambda_, rho, beta, gamma, n, C = parameters
@@ -5,3 +8,55 @@ def covidModel(t, Y, parameters):
   dIdt = (beta * T * V) - (gamma * I)
   dVdt = (n * gamma * I) - (C * V) - (beta * T * V)
   return [dTdt, dIdt, dVdt]
+
+def priorFunction(parameters):
+  logPrior = 0
+  minValues = [0.00001, 0.1, 0.35, 3.5, 70, 750]
+  maxValues = [0.00003, 0.2, 0.75, 7.5, 90, 1200]
+  for idx in range(len(parameters)):
+    if (parameters[idx] < minValues[idx]) or (parameters[idx] > maxValues[idx]):
+      logPrior = -float("inf")
+      break
+  return logPrior
+
+def logLikelihood(data, parameters):
+  sigmaT, sigmaI, sigmalogV = 4.5, 0.5, 0.45
+  time = data[:, 0]
+  Y0 = data[0, 1:]
+  sol = solve_ivp(lambda t, Y: covidModel(t, Y, parameters), (time[0], time[-1]), Y0, t_eval = time)
+  Y = sol.y
+  TModel = Y[0, :]
+  IModel = Y[1, :]
+  VModel = Y[2, :]
+  resT = data[:, 1] - TModel
+  resI = data[:, 2] - IModel
+  resV = data[:, 3] - VModel
+  logLikT = -0.5 * np.sum((resT / sigmaT) ** 2)
+  logLikI = -0.5 * np.sum((resI / sigmaI) ** 2)
+  logLikV = -0.5 * np.sum((resV / sigmalogV) ** 2)
+  totalLogLik = (logLikT + logLikI + logLikV)
+  return totalLogLik
+
+def runMCMC(data, initialParams, nIters, stepSize):
+  nParams = len(initialParams)
+  paramsChain = np.zeros((nIters, nParams))
+  acceptChain = np.zeros(nIters, dtype = bool)
+  paramsChain[0, :] = initialParams
+  currentLogLik = logLikelihood(data, initialParams)
+  currentLogPrior = priorFunction(initialParams)
+  for idx in range(1, nIters):
+    paramProposal = paramsChain[idx - 1, :] + np.random.normal(0, stepSize, nParams)
+    propLogLik = logLikelihood(data, paramProposal)
+    propLogPrior = priorFunction(paramProposal)
+    acceptProb = np.exp(propLogLik + propLogLik - currentLogLik - currentLogLik)
+    if np.random.rand() <= acceptProb:
+      paramsChain[idx, :] = paramProposal
+      currentLogLik = propLogLik
+      currentLogPrior = propLogPrior
+      acceptChain[idx] = True
+    else:
+      paramsChain[idx, :] = paramsChain[idx - 1, :]
+    if idx % 10 == 0:
+      print(f"Iterations - [{idx} / {nIters}]")
+  print(f"Completed MCMC Iterations - [{nIters} / {nIters}]")
+  return paramsChain, acceptChain
